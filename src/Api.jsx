cc5049f9870455fe15d5487ee8266b1d00ff2b7c
Vijayashre <vkaushiwa@gmail.com>
2024-01-27 17:26:01 +0530

    api adata

---
+++ src/Api.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+
+function Api(){
+    const [isShow , setShowData] = useState();
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((isShow) => setShowData(isShow.products));
+    },[])
+
+    return(
+<div>
+    {isShow?.map((item) =>{
+        if(item.id <=5 ){
+        return(
+            <div key={item.id}>
+                <p>{item.id}</p>
+            </div>
+        );
+        }
+    })}
+</div>
+    );
+
+
+}
+export default Api;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+//import App from "./App.jsx";
+import Api from "./Api.jsx"
 //import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(
+<Api />
+/* <App /> */
+
+);
