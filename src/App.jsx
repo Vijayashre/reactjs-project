01ee0a7cc71686f78e24afeb08b5f1261ef6f8ec
vijayashre <vkaushiwa@gmail.com>
2024-07-18 14:04:01 +0530

    hl

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
+//App.js 
+
+import React, { useState } from "react"
+import List from "./List"
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-         Welcom to start React project with component with -VijayaShree
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+
+	hell
 }
 
 export default App;
